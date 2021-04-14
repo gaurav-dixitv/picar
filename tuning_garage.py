6fa0b66f6005372e599808155b60caf54e6a3687
gaurav-dixitv <gaurav.dixitv@gmail.com>
2021-04-14 14:01:26 -0700

    Added reflection for 2.x

--- tuning_garage.py
+++ tuning_garage.py
@@ -2,6 +2,7 @@ from picar.libezblock import *
 from picar.picar import PiCar
 
 car = PiCar()
+
 car.forward(80)
 delay(500)
 car.backward(20)
@@ -9,3 +10,4 @@ delay(100)
 car.stop()
 
 car.forward(calibrate=False, angle=30)
+car.stop()
