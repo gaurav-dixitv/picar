dcd2d05136bc493c79c8d817cfdfe0589fab41c2
gaurav-dixitv <gaurav.dixitv@gmail.com>
2021-04-14 23:29:47 +0100

    Removed redundant imports.

--- picar/__init__.py
+++ picar/__init__.py
@@ -1,13 +1,2 @@
-import time
-try:
-    import ezblock as __ezb  # import private members namespaceded as __ezb
-    from ezblock import *
-except ImportError:
-    print("This computer does not appear to be a PiCar-X system. \n\
-        /opt/ezblock is not present.\n\
-        Shadowing hardware calls with stubs")
-    import picar.sim_ezblock as __ezb  # import private members namespaceded as __ezb
-    from picar.sim_ezblock import *
-finally:
-    __ezb.__reset_mcu__()
-    time.sleep(0.01)
+from .motorcontroller import MotorController
+from .picar import PiCar
